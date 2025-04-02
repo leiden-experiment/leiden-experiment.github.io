36186936ced6fb7ce7da02e616ec175d83729972
Martin <emailregisterlogin@gmail.com>
2025-04-02 19:59:59 +0200

    Update layer.ts

--- src/ts/level/layer.ts
+++ src/ts/level/layer.ts
@@ -211,9 +211,11 @@ export class PlayableLayer extends Layer {
       this.level.streak.check();
 
       if (config.sonificationEnabled) {
-        target.sound.play({
-          volume: config.pinchVolume,
-        });
+        if (target.scene !== undefined) {
+          target.sound.play({
+            volume: config.pinchVolume,
+          });
+        }
       }
 
       this.targetManager.destroyTarget(target);
